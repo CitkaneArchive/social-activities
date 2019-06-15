9a13cb4609224a6c08fdff7a32cc7e6caf5d3ad6
Michael Jonker <michael@openpoint.ie>
2019-06-15 16:37:08 +0100

    move start files to src

--- src/activities.js
+++ src/activities.js
@@ -3,8 +3,8 @@ const config = require('config');
 // eslint-disable-next-line no-underscore-dangle
 global.__network = config.get('network');
 
-const Sockets = require('../social-deployment/templates/nodejs/api/Sockets');
-const Api = require('./src/api/Api');
+const Sockets = require('../../social-deployment/templates/nodejs/api/Sockets');
+const Api = require('./api/Api');
 
 const sockets = new Sockets('activities');
 const api = new Api(sockets);
